e1c4c2bc56a1a22e8b035c50c409548c782e4f56
ain-0308 <172338794+ain-0308@users.noreply.github.com>
2024-11-08 11:47:07 +0900

    황아인/241108_11:46/이미지생성 모델 예외처리추가
    이미지 생성 실패 시 3번 시도 하도록 수정함

--- FastAPI/models/textToImage.py
+++ FastAPI/models/textToImage.py
@@ -29,27 +29,32 @@ def translate_kr_to_en(keyword_ko):
 
 def query(payload):
     response = requests.post(API_URL, headers=headers, json=payload)
-    print("이미지생성 확인 메세지 ", response)
+    print("이미지생성 확인 메시지 ", response)
     return response.content
 
 
-# 이미지 생성 함수
+#=================== 이미지 3가지 버전 생성 ======================
 def generate_images(keyword, styles):
     images = []
     for style in styles:
-        prompt = f"{keyword} in {style} style"
-        image_bytes = query({"inputs": prompt})
-        # 이미지가 유효한지 확인하는 부분 추가
-        if image_bytes:
-            images.append((style, image_bytes))
-        else:
-            print(f"이미지 생성 실패 스타일 : {style}")
-        # 각 이미지 생성 후 1분 대기 - 동시 생성 요청으로 인한 제한으로 추가함
-        # print("1분 대기 중...")
-        # # time.sleep(60)    
+        retry_count = 0
+        max_retries = 3  # 재시도 횟수 설정
+        while retry_count < max_retries:
+            # 이미지 생성 시도
+            prompt = f"{keyword} in {style} style"
+            image_bytes = query({"inputs": prompt})
+            # 이미지가 유효한지 확인하는 부분 추가
+            if image_bytes:
+                images.append((style, image_bytes))
+                break
+            else:
+                print(f"이미지 생성 실패 스타일 : {style}")
+                retry_count += 1
+        if retry_count == max_retries: # 재시도 후에도 실패시
+            print(f"생성 시도한 {style} 스타일 {max_retries}회 시도 후 실패 ")
     return images
 
-# 이미지를 생성하고 FastAPI로 전송하는 함수
+#================ 이미지를 생성하고 FastAPI로 전송하는 함수 ==============
 def generate_images_and_send(translated_text):
     print("이미지 생성 함수 시작")
     styles = ["watercolor", "comic", "photorealistic"]  # 스타일 리스트
